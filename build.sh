190f1b2d9da670558285ce602a8fbad6bbf6b08e
Emil Kozicki <emilex330@icloud.com>
2026-08-13 11:41:12 -0400

    Document email and contact variables in .env.example

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ set -o errexit
 pip install -r backend/requirements.txt
 python backend/manage.py collectstatic --no-input
 python backend/manage.py migrate
+python backend/manage.py createsuperuser --noinput || true
